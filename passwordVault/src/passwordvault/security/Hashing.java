1899c5b93b235019f1f2dade7fafbf5ce4176677
jccrosby1982 <jccrosby1982@yahoo.com>
2017-04-19 09:13:04 -0600

    Hashing updated

--- passwordVault/src/passwordvault/security/Hashing.java
+++ passwordVault/src/passwordvault/security/Hashing.java
@@ -1,47 +1,58 @@
-package passwordvault.security;
+package hashing; 
+
 import java.security.MessageDigest;
 import java.util.Scanner;
 import javax.xml.bind.DatatypeConverter;
 import java.io.*;
+import java.util.*;
+import java.nio.*;
+import java.nio.charset.*;
 
 public class Hashing {
 
-//    public static void main(String[] args) {
-//        simpleUI();
-//    }
+ /*   public static void main(String[] args) {
+        simpleUI();
+    }
     
     private static void simpleUI(){
         // GET STRING TO HASH
         Scanner sn = new Scanner(System.in);
         System.out.println("Please enter a password to be hashed");
-        String password = sn.nextLine();
+        char[] password1 = new char[1024];
+        password1 = sn.next().toCharArray();
         System.out.println("Please enter a filename for your file authentication");
-        String filename = sn.nextLine();
-        //CALL HASHING FUNCTION
-        Hashing sj = new Hashing();
-        String hash = sj.getStrHash(password);
-        String hash2 = sj.getFileHash(filename);
-        String temp = hash + hash2;
-        String hash3 = sj.getStrHash(temp);
+        String file1 = new String();
+        file1 = sn.next();
+        //CALL HASHING FUNCTIONM1
         //PRINT RESULTS
         System.out.println("This is the hash for your string using SHA-256!");
-        System.out.println(hash);
+        String hash1 = new String();
+        hash1 = getCharHash(password1);
+        System.out.println(hash1);
         System.out.println("this is the hash for your file using SHA-256!");
+        String hash2 = new String();
+        hash2 = getFileHash(file1);
         System.out.println(hash2);
-        System.out.println("This is the hash for both tokens combined using SHA-256!");
+        System.out.println("This is the final hash using SHA-256!");
+        String hash3 = new String();
+        hash3 = hash1 + hash2;
+        hash3 = getStrHash(hash3);
         System.out.println(hash3);
     }
-    public static String getStrHash(String password){
+ */   
+    public static String getCharHash(char[] password){
         String result = null;
         try{
             MessageDigest digest = MessageDigest.getInstance("SHA-256");
-            byte[] hash = digest.digest(password.getBytes("UTF-8"));
+            byte[] pwbytes = toBytes(password); 
+            byte[] hash = digest.digest(pwbytes);
+            Arrays.fill(pwbytes, (byte) 0); // clear sensitive data
             return DatatypeConverter.printHexBinary(hash);
         }catch(Exception ex){
            ex.printStackTrace();
         }
         return result;
-    }
+    }//*/
     
     public static String getFileHash(String filename){
         String result = null;
@@ -59,5 +70,25 @@ public class Hashing {
             ex.printStackTrace();
         }
         return result;
+    }
+    public static byte[] toBytes(char[] chars) {
+    CharBuffer charBuffer = CharBuffer.wrap(chars);
+    ByteBuffer byteBuffer = Charset.forName("UTF-8").encode(charBuffer);
+    byte[] bytes = Arrays.copyOfRange(byteBuffer.array(),
+            byteBuffer.position(), byteBuffer.limit());
+    Arrays.fill(charBuffer.array(), '\u0000'); // clear sensitive data
+    Arrays.fill(byteBuffer.array(), (byte) 0); // clear sensitive data
+    return bytes;
+}
+    public static String getStrHash(String password){
+        String result = null;
+        try{
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            byte[] hash = digest.digest(password.getBytes("UTF-8"));
+            return DatatypeConverter.printHexBinary(hash);
+        }catch(Exception ex){
+           ex.printStackTrace();
+        }
+    return result;
     }
 }

c92519f6abdaa12a3f6758e9b861c270431f32df
tb52s7 <TrogPhone@gmail.com>
2017-04-19 11:00:41 -0600

    finishing partial commit

---
+++ passwordVault/src/passwordvault/security/SecureString.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package passwordvault.security;
+
+/**
+ *
+ * @author Pope Trogodorius
+ */
+public class SecureString {
+    
+    // TO-DO:
+   
+    // 1. CREATE CONSTRUCTOR THAT INITS NEW CHAR[] OBJECTS
+    // 2. CREATE GET METHOD TO SEND CHAR[] DATA TO STRING-USING FUNCTIONS
+    // 3. CREATE RESET METHOD TO OVERWRITE CHAR[] DATA
+    // 4. CREATE CLOSE METHOD TO CALL RESET WHEN CHAR[] NO LONGER NEEDED
+    
+}
